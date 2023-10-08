018094d5c4af45b7710a1fb159375abd7cbc714f
FelipeFalcaocg <felipefalcaocg@hotmail.com>
2023-10-08 18:10:13 -0300

    oi

--- structures/model/node.py
+++ structures/model/node.py
@@ -1,47 +1,50 @@
 import operator
 from functools import reduce
+
 from geom2d import Point, Vector
 
+
 class StrNode:
-    def __init__(
-    self,
-    _id: int,
-    position: Point,
-    loads=None,
-    dx_constrained=False,
-    dy_constrained=False
-):
 
-    self.id = _id
-    self.position = position
-    self.loads = loads or []
-    self.dx_constrained = dx_constrained
-    self.dy_constrained = dy_constrained
+    def __init__(   
+            self,
+            _id: int,
+            position: Point,
+            loads=None,
+            dx_constrained=False,
+            dy_constrained=False
+    ):
 
-@property
-def loads_count(self):
-    return len(self.loads)
+        self.id = _id
+        self.position = position
+        self.loads = loads or []
+        self.dx_constrained = dx_constrained
+        self.dy_constrained = dy_constrained
 
-@property
-def net_load(self):
-    return reduce(
-        operator.add,
-        self.loads,
-        Vector(0, 0)
-)
+    @property
+    def loads_count(self):
+        return len(self.loads)
 
-def add_load(self, load: Vector):
-    self.loads.append(load)
+    @property
+    def net_load(self):
+        return reduce(
+            operator.add,
+            self.loads,
+            Vector(0, 0)
+        )
 
-def __eq__(self, other):
-    if self is other:
-        return True
-        
-    if not isinstance(other, StrNode):
-        return False
+    def add_load(self, load: Vector):
+        self.loads.append(load)
 
-    return self.position == other.position
+    def __eq__(self, other):
+        if self is other:
+            return True
+            
+        if not isinstance(other, StrNode):
+            return False
+
+        return self.position == other.position
 
 
-    
+
 
